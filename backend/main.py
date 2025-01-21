84bf1a9513c234ccc3ae378ed5b20dbab8985dfc
iywq <ivan.yeowq@gmail.com>
2025-01-21 13:42:19 +0800

    Reorganised preprocessing files, refactored code

---
+++ backend/main.py
@@ -0,0 +1,42 @@
+import argparse
+# from pdf_splitting import process_all_pdfs_in_folder
+# from document_translator import process_and_combine
+# from combine_extracted_csv import combine_csv_files
+from datetime import datetime
+
+from ocr import generate_ocr_files
+
+
+
+
+def main(input_dir, output_dir):
+    ocr_output_dir = f"{output_dir}"
+
+    start_time = datetime.now()
+
+    generate_ocr_files(input_dir, ocr_output_dir)
+    print(f"Processed data saved to {ocr_output_dir}")
+    print(f"Start time: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    end_time = datetime.now()
+    print(f"End time: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some files.")
+    parser.add_argument(
+        "--input_dir",
+        default="sample/anti_money_laundering",
+        type=str,
+        help="The input directory where your raw pdf files will be stored.",
+    )
+    parser.add_argument(
+        "--output_dir",
+        default="pre_processing/AML_output",
+        type=str,
+        help="The output directory where the extracted.csv files will be stored.",
+    )
+    args = parser.parse_args()
+
+    main(args.input_dir, args.output_dir)
--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -1,29 +1,31 @@
 import os
 import re
 
+
 def clean_text(text):
     # Remove non-ASCII characters
-    text = re.sub(r'[^\x00-\x7F]+', ' ', text)
+    text = re.sub(r"[^\x00-\x7F]+", " ", text)
     # Remove URLs
-    text = re.sub(r'http\S+', '', text)
+    text = re.sub(r"http\S+", "", text)
     # Remove special characters and numbers
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = re.sub(r"[^a-zA-Z\s]", "", text)
     # Convert to lowercase
     text = text.lower()
     # Remove extra whitespace
-    text = re.sub(r'\s+', ' ', text).strip()
+    text = re.sub(r"\s+", " ", text).strip()
     # Remove extra newlines
-    text = re.sub(r'\n+', '\n', text)
+    text = re.sub(r"\n+", "\n", text)
     return text
+
 
 def clean_text_files(folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith(".txt"):
             txt_path = os.path.join(folder_path, filename)
-            with open(txt_path, 'r') as f:
+            with open(txt_path, "r") as f:
                 text = f.read()
             cleaned_text = clean_text(text)
             cleaned_txt_path = txt_path.replace(".txt", "_cleaned.txt")
-            with open(cleaned_txt_path, 'w') as f:
+            with open(cleaned_txt_path, "w") as f:
                 f.write(cleaned_text)
             print(f"Cleaned text in {filename} and saved to {cleaned_txt_path}")
