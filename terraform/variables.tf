bc4a4a4bff75226a6eb82798235897389ecd3a88
nicamanas <xyukimiku@gmail.com>
2025-02-10 01:49:52 +0800

    test fix 3 for missing region var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,16 +4,6 @@ variable "region" {
   default     = "ap-southeast-1"
 }
 
-# variable "aws_access_key" {
-#   description = "The AWS access key"
-#   type        = string
-# }
-
-# variable "aws_secret_key" {
-#   description = "The AWS secret key"
-#   type        = string
-# }
-
 variable "s3_name" {
   description = "The name of the S3 bucket to create"
   type        = string
