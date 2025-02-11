91df44410ea7f791a24aca5e5025d1580fc7dfc0
nicamanas <xyukimiku@gmail.com>
2025-02-11 23:08:01 +0800

    fix s3 settings + next js export

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,12 +1,12 @@
 // use this for creating a new bucket
-resource "aws_s3_bucket" "s3" {
-  bucket = var.project_name
-}
+# resource "aws_s3_bucket" "s3" {
+#   bucket = var.project_name
+# }
 
 // use this for using an existing bucket
-# data "aws_s3_bucket" "s3" {
-#   bucket = "lsm-fyp"
-# }
+data "aws_s3_bucket" "s3" {
+  bucket = "lsm-fyp"
+}
 
 resource "aws_s3_bucket_website_configuration" "this" {
   bucket = aws_s3_bucket.s3.bucket
@@ -16,17 +16,17 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 
   error_document {
-    key = "index.html"
+    key = "404.html"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "s3" {
   bucket = aws_s3_bucket.s3.bucket
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "s3" {
