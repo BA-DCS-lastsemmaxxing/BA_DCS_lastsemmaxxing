7567e0f5b38b078a727656515d0f9a841b77cdbf
nicamanas <xyukimiku@gmail.com>
2025-02-11 18:22:50 +0800

    test fix for acl policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,7 +4,15 @@ resource "aws_s3_bucket" "s3" {
 
 resource "aws_s3_bucket_acl" "s3" {
   bucket = aws_s3_bucket.s3.id
-  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3" {
+  bucket = aws_s3_bucket.s3.id
+
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "s3" {
