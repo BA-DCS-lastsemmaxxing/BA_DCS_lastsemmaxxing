346101c3d18937a76ae8dd5ed5b7e39b025b64e8
nicamanas <xyukimiku@gmail.com>
2025-02-16 12:12:16 +0800

    fix missing default vpc

--- terraform/database.tf
+++ terraform/database.tf
@@ -10,10 +10,34 @@ resource "aws_db_instance" "rds" {
     skip_final_snapshot = true
 
     vpc_security_group_ids = [aws_security_group.rds_sg.id]
+    db_subnet_group_name = aws_db_subnet_group.default.name
 
     tags = {
         Name = "lsm-fyp-rds"
     }
+}
+
+# Fetch the default VPC
+data "aws_vpc" "default" {
+    default = true
+}
+
+# Fetch the default subnet in the default VPC
+data "aws_subnet" "default" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
+# Create a DB subnet group using the default VPC subnets
+resource "aws_db_subnet_group" "default" {
+  name       = "default-subnet-group"
+  subnet_ids = [data.aws_subnet.default.id]
+
+  tags = {
+    Name = "Default DB Subnet Group"
+  }
 }
 
 resource "aws_security_group" "rds_sg" {
