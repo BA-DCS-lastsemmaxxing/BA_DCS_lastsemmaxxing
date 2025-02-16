741032a222e7ebb2397c1859dd3f4503f2ab9320
Khidir123 <khidirla.2022@scis.smu.edu.sg>
2025-02-16 10:08:16 +0800

    merge branch

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   /* config options here */
   output: "export",
-=======
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
->>>>>>> dockerise
 };
 
 export default nextConfig;
