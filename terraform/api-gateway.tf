26468a026df63331864def2ebaca24d06f4fdbd4
nicamanas <xyukimiku@gmail.com>
2025-02-22 23:55:34 +0800

    fixes for cf and apigw errors in prev deployment

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -19,17 +19,38 @@ resource "aws_api_gateway_deployment" "prod_deployment" {
     lifecycle {
         create_before_destroy = true
     }
+
+    # edit this for the methods/integrations for api gateway
+    depends_on = [ aws_api_gateway_method.test_method, aws_api_gateway_method_integration.get_users_integration ]
 }
 
-resource "aws_api_gateway_resource" "api_resource" {
+# use these to create endpoints
+# create a resource for the API (e.g /users)
+resource "aws_api_gateway_resource" "test_resource" {
     rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
     parent_id   = aws_api_gateway_rest_api.lsm-fyp-api.root_resource_id
-    path_part   = "data"
+    path_part   = "test"
 }
 
-resource "aws_api_gateway_method" "api_method" {
+# create a GET method for the /test resource
+resource "aws_api_gateway_method" "test_method" {
     rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-    resource_id   = aws_api_gateway_resource.api_resource.id
+    resource_id   = aws_api_gateway_resource.test_resource.id
     http_method   = "GET"
     authorization = "AWS_IAM"
+}
+
+# Create an integration for the GET method (e.g., Mock Integration)
+resource "aws_api_gateway_method_integration" "get_users_integration" {
+  rest_api_id = aws_api_gateway_rest_api.lsm_fyp_api.id
+  resource_id = aws_api_gateway_resource.users_resource.id
+  http_method = aws_api_gateway_method.get_users.http_method
+
+  integration_http_method = "GET"
+  type                    = "MOCK"  # Or "AWS", "HTTP" depending on your use case
+
+  # Optional: Set the request/response templates
+  request_templates = {
+    "application/json" = jsonencode({ "statusCode": 200 })
+  }
 }
