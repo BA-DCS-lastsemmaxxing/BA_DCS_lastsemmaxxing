942869441d1b62e8b7f5594ba9eb07a03ba3a924
nicamanas <xyukimiku@gmail.com>
2025-02-24 11:12:47 +0800

    added oai to cf (#5)
    fix for 403 forbidden errors

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 // use this for creating a new bucket - fixed with cloud usage
-resource "aws_s3_bucket" "s3" {
+resource "aws_s3_bucket" "s3_frontend" {
   bucket = "${var.project_name}-frontend"
 }
 
@@ -8,8 +8,8 @@ resource "aws_s3_bucket" "s3" {
 #   bucket = var.project_name
 # }
 
-resource "aws_s3_bucket_website_configuration" "this" {
-  bucket = aws_s3_bucket.s3.bucket
+resource "aws_s3_bucket_website_configuration" "s3_frontend_website_config" {
+  bucket = aws_s3_bucket.s3_frontend.bucket
   // bucket = data.aws_s3_bucket.s3.id
 
   index_document {
@@ -21,32 +21,39 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "s3" {
-  bucket = aws_s3_bucket.s3.bucket
-  // bucket = data.aws_s3_bucket.s3.id
+# resource "aws_s3_bucket_public_access_block" "s3" {
+#   bucket = aws_s3_bucket.s3.bucket
+#   // bucket = data.aws_s3_bucket.s3.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_policy" "s3" {
-  bucket = aws_s3_bucket.s3.bucket
-  // bucket = data.aws_s3_bucket.s3.id
+resource "aws_s3_bucket_policy" "s3_frontend_bucket_policy" {
+  bucket = aws_s3_bucket.s3_frontend.bucket
 
   policy = jsonencode({
     Version = "2012-10-17"
     Id      = "AllowGetObjects"
     Statement = [
       {
-        Sid       = "AllowPublic"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.s3.arn}/**"
-        # Resource  = "${data.aws_s3_bucket.s3.arn}/**"
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.s3_frontend.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.cdn.id}"
+          }
+        }
       }
     ]
   })
 }
+
+data "aws_caller_identity" "current" {}
