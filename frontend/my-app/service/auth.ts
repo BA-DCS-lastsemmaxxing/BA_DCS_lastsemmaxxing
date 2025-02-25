ca173f0aa089b0bf9ab2192723e73f44a17e030e
seldomearly69 <130273874+seldomearly69@users.noreply.github.com>
2025-02-25 23:25:33 +0800

    storing token in cookies to feed lambda authoriser

--- frontend/my-app/service/auth.ts
+++ frontend/my-app/service/auth.ts
@@ -25,7 +25,7 @@ export const auth = {
 
       cognitoUser.authenticateUser(authenticationDetails, {
         onSuccess: (result) => {
-            document.cookie = `CognitoToken=${result.getIdToken().getJwtToken()}; path=/; Secure; HttpOnly`;
+            document.cookie = `CognitoToken=${result.getIdToken().getJwtToken()}; path=/; Secure; SameSite=None`;
           resolve(result);
         },
         onFailure: (err) => {
