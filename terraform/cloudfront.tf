ed13105b83ddb91712f0ebdec2a1d0d9b1d71699
nicamanas <xyukimiku@gmail.com>
2025-02-28 19:40:21 +0800

    fix typo

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -109,7 +109,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     cached_methods   = ["GET", "HEAD"]
 
     cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
-    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.managed_origin_request_policy
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.managed_origin_request_policy.id
 
     forwarded_values {
       query_string = true
