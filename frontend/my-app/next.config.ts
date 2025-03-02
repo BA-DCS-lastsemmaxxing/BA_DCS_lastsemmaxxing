ec3ded338749874c7c2ecadfbe49ce5f0a7de3e7
seldomearly69 <130273874+seldomearly69@users.noreply.github.com>
2025-03-02 16:03:32 +0800

    Revert "attempting to fix xml access denied"
    This reverts commit 049a7daa4d13706463d73b153c985de5873a6d10.

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -3,18 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  trailingSlash: false,
-  images: {
-    unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/_next/static/chunks/pages/:path*.js',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
