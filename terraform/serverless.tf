d54ed6a0d251afe053c9109ddab11a9bf107082f
nicamanas <xyukimiku@gmail.com>
2025-03-03 17:29:04 +0800

    revert previous change

--- terraform/serverless.tf
+++ terraform/serverless.tf
@@ -60,7 +60,6 @@ resource "aws_lambda_function" "fetch_documents_lambda" {
 
   s3_bucket = "${var.project_name}-serverless-ap"
   s3_key = "fetch_documents.zip"
-  source_code_hash = filebase64sha256("fetch_documents.zip") # Ensure redeployment when the file changes
 
   role = aws_iam_role.lambda_execution_role.arn
 }
@@ -74,7 +73,6 @@ resource "aws_lambda_function" "upload_document_lambda" {
 
   s3_bucket = "${var.project_name}-serverless-ap"
   s3_key = "upload_document.zip"
-  source_code_hash = filebase64sha256("upload_document.zip")
 
   role = aws_iam_role.lambda_execution_role.arn
 
@@ -143,6 +141,5 @@ resource "aws_lambda_function" "auth_lambda_edge" {
     handler = "auth_lambda.lambda_handler"
     runtime = "python3.8"
     publish = true
-    source_code_hash = filebase64sha256("auth_lambda.zip") # Ensure redeployment when the file changes
 }
 
