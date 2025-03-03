f9f93ed396923cc62be58085ac0ecb3bdcc37efe
nicamanas <xyukimiku@gmail.com>
2025-03-03 16:51:31 +0800

    Upload functions (#7)
    * add cors support to options endpoints
    
    * test disable cors options endpoints
    
    * create new bucket to separate us/ap serverless
    
    * new serverless functions + pipeline update
    
    * terraform changes for new lambda functions
    
    * fix lambda policy resource arn
    
    * add basic lambda execution role

---
+++ serverless/fetch_documents.py
@@ -0,0 +1,19 @@
+import json
+from models import Document  # Import the shared models.py
+
+def lambda_handler(event, context):
+    
+    # Extract query parameter
+    query = event.get("queryStringParameters", {}).get("query", None)
+    
+    print(f"Query: {query}", flush=True)
+
+    # Fetch documents from database
+    documents = Document.get_documents(query)
+    print("documents: ", documents, flush=True)
+    # Return a valid API Gateway response
+    return {
+        "statusCode": 200,
+        "headers": { "Content-Type": "application/json" },
+        "results": json.dumps(documents)
+    }
