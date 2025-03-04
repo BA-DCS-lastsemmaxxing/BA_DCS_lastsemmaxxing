bd2f9fc714d54b60d7e9ead50867a59b6dcc521d
seldomearly69 <130273874+seldomearly69@users.noreply.github.com>
2025-03-04 15:56:20 +0800

    attempting to fix refresh issue

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
+  trailingSlash: true, // Ensures Next.js exports pages with a trailing slash
+  distDir: "out",
 };
 
 export default nextConfig;
