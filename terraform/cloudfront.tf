d1775054e93ed6132f0c8b9bc4f5d721a410ddd2
nicamanas <xyukimiku@gmail.com>
2025-03-05 01:46:34 +0800

    add cache policy

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -90,6 +90,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     allowed_methods   = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods    = ["GET", "HEAD"]
 
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
     origin_request_policy_id = "Managed-AllViewer"
 
     viewer_protocol_policy = "redirect-to-https"
