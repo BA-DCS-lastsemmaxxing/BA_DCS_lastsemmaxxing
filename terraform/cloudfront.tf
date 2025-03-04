001ceecab45d87f35fa204f757b5dba7631824c1
nicamanas <xyukimiku@gmail.com>
2025-03-05 01:27:52 +0800

    fix request policy

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -60,6 +60,27 @@ data "aws_cloudfront_origin_request_policy" "managed_origin_request_policy" {
   name = "Managed-AllViewerExceptHostHeader"
 }
 
+resource "aws_cloudfront_origin_request_policy" "auth_cookie_policy" {
+  name    = "${var.project_name}-auth-cookie-policy"
+  comment = "Forwards Cognito authentication cookies and authorization header to API Gateway"
+
+  cookies_config {
+    cookie_behavior = "all"  # Forward all cookies
+  }
+
+  headers_config {
+    header_behavior = "whitelist"
+    headers {
+      items = ["Authorization", "Cookie"]  # Whitelist the headers you want to forward
+    }
+  }
+
+  query_strings_config {
+    query_string_behavior = "all"  # Forward all query strings
+  }
+}
+
+
 resource "aws_cloudfront_distribution" "cdn" {
   
   enabled = true
@@ -91,15 +112,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     allowed_methods   = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods    = ["GET", "HEAD"]
 
-    forwarded_values {
-      query_string = true
-      cookies {
-        forward = "all"
-      }
-      headers = {
-        items = ["Authorization", "Cookie"]
-      }
-    }
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.auth_cookie_policy.id
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
