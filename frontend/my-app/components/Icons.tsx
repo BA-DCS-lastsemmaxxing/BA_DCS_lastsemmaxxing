6a48a4f9239e4bd53f45688b810833feb7c853fd
seldomearly69 <130273874+seldomearly69@users.noreply.github.com>
2025-03-05 19:17:11 +0800

    push

--- frontend/my-app/components/Icons.tsx
+++ frontend/my-app/components/Icons.tsx
@@ -3,11 +3,13 @@ import{
     User,
     ChevronDown,
     FilePlus2,
+    Download
 } from "lucide-react";
 
 export const Icons: any = {
     Menu: Menu,
     User: User,
     ChevronDown: ChevronDown,
-    NewDocument: FilePlus2
+    NewDocument: FilePlus2,
+    Download: Download
 }
