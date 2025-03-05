5d043f4043c0abef4f42515e5fc75461cbc53408
nicamanas <xyukimiku@gmail.com>
2025-03-06 01:30:25 +0800

    apply cache and origin request policy

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -90,13 +90,8 @@ resource "aws_cloudfront_distribution" "cdn" {
     allowed_methods   = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods    = ["GET", "HEAD"]
 
-    forwarded_values {
-      headers = ["Authorization", "Host"]
-      query_string = true
-      cookies {
-        forward = "all"
-      }
-    }
+    cache_policy_id = data.aws_cloudfront_cache_policy.caching_disabled.id
+    origin_request_policy_id = data.aws_cloudfront_origin_request_policy.managed_origin_request_policy.id
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
