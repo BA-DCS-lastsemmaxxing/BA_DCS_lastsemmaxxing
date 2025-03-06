ab048a29a744f9fc0e14cae371c286bc41460d48
nicamanas <xyukimiku@gmail.com>
2025-03-06 20:15:10 +0800

    change api gw authorizer identity source

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -14,6 +14,7 @@ resource "aws_api_gateway_authorizer" "lsm-fyp-authorizer" {
     rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
     type = "COGNITO_USER_POOLS"
     provider_arns = [aws_cognito_user_pool.lsm-fyp-user-pool.arn]
+    identity_source = "method.request.header.Cookie"
 }
 
 resource "aws_api_gateway_deployment" "prod_deployment" {
