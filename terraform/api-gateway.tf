4883954f8bd3a192843516e04273e6d8a289c9c7
nicamanas <xyukimiku@gmail.com>
2025-03-06 12:30:55 +0800

    test api gw fix

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -111,18 +111,18 @@ resource "aws_api_gateway_method" "documents_method_options" {
     authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "documents_method_options_response" {
-    rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-    resource_id = aws_api_gateway_resource.documents_resource.id
-    http_method = aws_api_gateway_method.documents_method_options.http_method
-    status_code = "200"
-    
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Origin" = true
-        "method.response.header.Access-Control-Allow-Methods" = true
-        "method.response.header.Access-Control-Allow-Headers" = true
-        "method.response.header.Access-Control-Allow-Credentials" = true
-    }
+resource "aws_api_gateway_integration_response" "documents_method_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
+  resource_id = aws_api_gateway_resource.documents_resource.id
+  http_method = aws_api_gateway_method.documents_method_options.http_method
+  status_code = "200"
+  
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET, POST, OPTIONS'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token, X-XSRF-TOKEN, Access-Control-Allow-Headers, Access-Control-Allow-Origin'"
+    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+  }
 }
 
 resource "aws_api_gateway_integration" "documents_options_integration" {
