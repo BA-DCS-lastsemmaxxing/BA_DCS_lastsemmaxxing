f445006a0ac5635e617bfae420e978eea39659b6
Khidir123 <khidirla.2022@scis.smu.edu.sg>
2025-03-08 17:34:02 +0800

    test

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,246 +1,61 @@
-resource "aws_api_gateway_rest_api" "lsm-fyp-api" {
-  name        = "${var.project_name}-apigw"
-  description = "API Gateway for ${var.project_name}"
+resource "aws_api_gateway_rest_api" "lsm_fyp_api" {
+  name        = "lsm-fyp-api"
+  description = "API Gateway for LSM FYP Project"
 }
 
-resource "aws_api_gateway_stage" "prod" {
-  stage_name    = "prod"
-  rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-  deployment_id = aws_api_gateway_deployment.prod_deployment.id
+resource "aws_api_gateway_resource" "delete_document" {
+  rest_api_id = aws_api_gateway_rest_api.lsm_fyp_api.id
+  parent_id   = aws_api_gateway_rest_api.lsm_fyp_api.root_resource_id
+  path_part   = "documents"  # This is the endpoint part
 }
 
-resource "aws_api_gateway_authorizer" "lsm-fyp-authorizer" {
-  name          = "${var.project_name}-authorizer"
-  rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-  type          = "COGNITO_USER_POOLS"
-  provider_arns = [aws_cognito_user_pool.lsm-fyp-user-pool.arn]
-}
-
-resource "aws_api_gateway_deployment" "prod_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  triggers = {
-    redeployment = sha1(jsonencode(
-      aws_api_gateway_rest_api.lsm-fyp-api
-    ))
-  }
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    aws_api_gateway_method.documents_method_get,
-    aws_api_gateway_method.documents_method_options
-  ]
-}
-
-# Create a resource for the API (e.g /documents)
-resource "aws_api_gateway_resource" "documents_resource" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  parent_id   = aws_api_gateway_rest_api.lsm-fyp-api.root_resource_id
-  path_part   = "documents"
-}
-
-# GET for /documents
-resource "aws_api_gateway_method" "documents_method_get" {
-  rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id   = aws_api_gateway_resource.documents_resource.id
-  http_method   = "GET"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.lsm-fyp-authorizer.id
-}
-
-# Trigger fetch documents lambda function when GET method called on /documents
-resource "aws_api_gateway_integration" "documents_method_get_integration" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.documents_resource.id
-  http_method = aws_api_gateway_method.documents_method_get.http_method
-  
-  integration_http_method = "POST" # Always POST for Lambda proxy integration
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.fetch_documents_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "documents_method_get_lambda_permission" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fetch_documents_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.lsm-fyp-api.execution_arn}/*/*"
-}
-
-# Create /upload resource
-resource "aws_api_gateway_resource" "upload_resource" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  parent_id   = aws_api_gateway_rest_api.lsm-fyp-api.root_resource_id
-  path_part   = "upload"
-}
-
-# Create /upload/url resource under /upload
-resource "aws_api_gateway_resource" "upload_url_resource" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  parent_id   = aws_api_gateway_resource.upload_resource.id
-  path_part   = "url"
-}
-
-resource "aws_api_gateway_method" "upload_url_method_get" {
-  rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id   = aws_api_gateway_resource.upload_url_resource.id
-  http_method   = "GET"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.lsm-fyp-authorizer.id
-}
-
-resource "aws_api_gateway_integration" "upload_url_method_get_integration" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.upload_url_resource.id
-  http_method = aws_api_gateway_method.upload_url_method_get.http_method
-  
-  integration_http_method = "POST" # Always POST for Lambda proxy integration
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.fetch_upload_url_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "upload_url_method_get_lambda_permission" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.fetch_upload_url_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.lsm-fyp-api.execution_arn}/*/*"
-}
-
-resource "aws_api_gateway_method" "upload_url_method_options" {
-  rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id   = aws_api_gateway_resource.upload_url_resource.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "upload_url_method_options_response" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.upload_url_resource.id
-  http_method = aws_api_gateway_method.upload_url_method_options.http_method
-  status_code = "200"
-  
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Credentials" = true
-  }
-}
-
-resource "aws_api_gateway_integration" "upload_url_options_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id             = aws_api_gateway_resource.upload_url_resource.id
-  http_method             = aws_api_gateway_method.upload_url_method_options.http_method
-  type                    = "MOCK"
-  request_templates = {
-    "application/json" = jsonencode({
-      statusCode = 200
-    })
-  }
-}
-
-resource "aws_api_gateway_integration_response" "upload_url_options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.upload_url_resource.id
-  http_method = aws_api_gateway_method.upload_url_method_options.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
-    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
-  }
-  
-  depends_on = [aws_api_gateway_integration.upload_url_options_integration]
-}
-
-# DELETE for /documents/{document_id}
 resource "aws_api_gateway_method" "delete_document_method" {
-  rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id   = aws_api_gateway_resource.documents_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.lsm_fyp_api.id
+  resource_id   = aws_api_gateway_resource.delete_document.id
   http_method   = "DELETE"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.lsm-fyp-authorizer.id
+  authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "delete_document_method_integration" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.documents_resource.id
+  rest_api_id = aws_api_gateway_rest_api.lsm_fyp_api.id
+  resource_id = aws_api_gateway_resource.delete_document.id
   http_method = aws_api_gateway_method.delete_document_method.http_method
-  
-  integration_http_method = "POST" # Always POST for Lambda proxy integration
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.delete_document_lambda.invoke_arn
-}
-
-resource "aws_lambda_permission" "delete_document_method_lambda_permission" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.delete_document_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.lsm-fyp-api.execution_arn}/*/*"
+  type         = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri         = "arn:aws:apigateway:ap-southeast-1:lambda:path/2015-03-31/functions/${aws_lambda_function.delete_document_lambda_new.arn}/invocations"
 }
 
 resource "aws_api_gateway_method_response" "delete_document_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.documents_resource.id
+  rest_api_id = aws_api_gateway_rest_api.lsm_fyp_api.id
+  resource_id = aws_api_gateway_resource.delete_document.id
   http_method = aws_api_gateway_method.delete_document_method.http_method
   status_code = "200"
-  
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Credentials" = true
-  }
 }
 
-resource "aws_api_gateway_method" "documents_method_options" {
-  rest_api_id   = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id   = aws_api_gateway_resource.documents_resource.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "documents_method_options_response" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.documents_resource.id
-  http_method = aws_api_gateway_method.documents_method_options.http_method
+resource "aws_api_gateway_integration_response" "delete_document_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.lsm_fyp_api.id
+  resource_id = aws_api_gateway_resource.delete_document.id
+  http_method = aws_api_gateway_method.delete_document_method.http_method
   status_code = "200"
-  
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Credentials" = true
+  response_templates = {
+    "application/json" = ""
   }
 }
 
-resource "aws_api_gateway_integration" "documents_options_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id             = aws_api_gateway_resource.documents_resource.id
-  http_method             = aws_api_gateway_method.documents_method_options.http_method
-  type                    = "MOCK"
-  request_templates = {
-    "application/json" = jsonencode({
-      statusCode = 200
-    })
-  }
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.lsm_fyp_api.id
+  stage_name  = "prod"  # You can also create a stage for test or dev
 }
 
-resource "aws_api_gateway_integration_response" "documents_method_options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.lsm-fyp-api.id
-  resource_id = aws_api_gateway_resource.documents_resource.id
-  http_method = aws_api_gateway_method.documents_method_options.http_method
-  status_code = "200"
-  
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS,DELETE'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
-    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
-  }
+resource "aws_api_gateway_stage" "prod_stage" {
+  stage_name    = "prod"
+  rest_api_id   = aws_api_gateway_rest_api.lsm_fyp_api.id
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.delete_document_lambda_new.function_name
+  principal     = "apigateway.amazonaws.com"
 }
--- terraform/serverless.tf
+++ terraform/serverless.tf
@@ -4,17 +4,16 @@ resource "aws_iam_role" "lambda_execution_role" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
-        },
+        }
         Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-# Allow Lambda to access other AWS resources
 resource "aws_iam_policy" "lambda_policy" {
   name        = "${var.project_name}-lambda-policy"
   description = "Allow Lambda to access other AWS resources"
@@ -23,7 +22,7 @@ resource "aws_iam_policy" "lambda_policy" {
     Statement = [
       # S3 and RDS access
       {
-        Effect   = "Allow",
+        Effect   = "Allow"
         Action   = [
           "s3:GetObject",
           "s3:PutObject",
@@ -38,7 +37,7 @@ resource "aws_iam_policy" "lambda_policy" {
           "rds-data:CommitTransaction",
           "rds-data:RollbackTransaction",
           "execute-api:Invoke"
-        ],
+        ]
         Resource = [
           "${aws_s3_bucket.serverless_bucket_ap.arn}/*",
           "${aws_s3_bucket.document_storage_bucket.arn}/*",
@@ -48,19 +47,18 @@ resource "aws_iam_policy" "lambda_policy" {
       },
       # CloudWatch Logs Access
       {
-        Effect   = "Allow",
+        Effect   = "Allow"
         Action   = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents"
-        ],
+        ]
         Resource = "arn:aws:logs:ap-southeast-1:874280117166:*"
       }
     ]
   })
 }
 
-# Lambda Functions
 resource "aws_lambda_function" "fetch_documents_lambda" {
   function_name = "fetch_documents"
   runtime       = "python3.9"
@@ -97,7 +95,7 @@ resource "aws_lambda_function" "fetch_upload_url_lambda" {
   }
 }
 
-resource "aws_lambda_function" "delete_document_lambda" {
+resource "aws_lambda_function" "delete_document_lambda_new" {  # Renamed
   function_name = "delete_document"
   runtime       = "python3.9"
   handler       = "delete_document.lambda_handler"
@@ -113,8 +111,7 @@ resource "aws_lambda_function" "delete_document_lambda" {
   }
 }
 
-# Attach the policy to the role
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment_new" {  # Renamed
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
@@ -122,60 +119,4 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Code for Lambda Edge Function - Lambda Authoriser
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
-
-resource "aws_iam_role" "lambda_edge_role" {
-  provider = aws.us-east-1
-  name     = "lambda-edge-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "edgelambda.amazonaws.com"
-        }
-      },
-      {
-        Effect = "Allow"
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "lambda_edge_policy_attach" {
-  provider   = aws.us-east-1
-  name       = "lambda-edge-policy-attach"
-  roles      = [aws_iam_role.lambda_edge_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_function" "auth_lambda_edge" {
-  provider   = aws.us-east-1
-  s3_bucket  = "${var.project_name}-serverless-us"
-  s3_key     = "auth_lambda.zip"
-  function_name = "auth_lambda_edge"
-  role          = aws_iam_role.lambda_edge_role.arn
-  handler       = "auth_lambda.lambda_handler"
-  runtime       = "python3.9"
-  publish       = true
-  source_code_hash = data.aws_s3_object.auth_lambda_zip.etag
 }
