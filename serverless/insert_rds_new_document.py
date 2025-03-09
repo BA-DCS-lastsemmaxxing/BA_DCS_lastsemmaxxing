f96731f20aa984b4083b70f733dd0e52f58d939f
seldomearly69 <130273874+seldomearly69@users.noreply.github.com>
2025-03-09 18:00:15 +0800

    Update insert_rds_new_document.py

--- serverless/insert_rds_new_document.py
+++ serverless/insert_rds_new_document.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "headers": {
-                "Access-Control-Allow-Origin": origin,
+                "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "OPTIONS, POST",
                 "Access-Control-Allow-Headers": "Content-Type, Authorization"
             },
