6082ae40e32bb263415d0d74a709529a8d3a5d34
Khidir123 <khidirla.2022@scis.smu.edu.sg>
2025-03-09 18:58:49 +0800

    add s3 object

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -129,3 +129,8 @@ data "aws_s3_object" "insert_rds_new_document_lambda_zip" {
   bucket = aws_s3_bucket.serverless_bucket_ap.bucket
   key = "insert_rds_new_document.zip"
 }
+
+data "aws_s3_object" "deleteDocumentFunction_lambda_zip" {
+  bucket = aws_s3_bucket.serverless_bucket_ap.bucket
+  key = "deleteDocumentFunction_document.zip"
+}
