03232eaca4119c25e5b1db21a3332d6c99aea07b
Khidir123 <khidirla.2022@scis.smu.edu.sg>
2025-03-11 18:34:26 +0800

    update database schema

--- mysql/lsm_fyp.sql
+++ mysql/lsm_fyp.sql
@@ -31,16 +31,18 @@ DROP TABLE IF EXISTS `documents`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `documents` (
-  `id` VARCHAR(36) NOT NULL,
+  `id` varchar(36) NOT NULL,
   `name` varchar(255) NOT NULL,
   `uploadedAt` datetime DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
   `summary` text,
   `topics` json DEFAULT NULL,
   `classification` varchar(50) DEFAULT NULL,
-  `confidence` DECIMAL(18, 16) DEFAULT NULL,
+  `confidence` decimal(18,16) DEFAULT NULL,
+  `user_corrected_category` varchar(255) DEFAULT NULL,
+  `feedback` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,7 +51,7 @@ CREATE TABLE `documents` (
 
 LOCK TABLES `documents` WRITE;
 /*!40000 ALTER TABLE `documents` DISABLE KEYS */;
-INSERT INTO `documents` VALUES (1,'sample','2024-01-25 15:44:00','processing',NULL,NULL,NULL),(2,'sample2.pdf','2024-01-25 15:44:00','completed','This appears to be a financial report for the year 2024','[\"Financial\", \"Report\", \"2024\"]',NULL),(7,'626 Banks_GCO vetted.pdf','2025-02-25 14:08:29','completed','The document provided is a comprehensive guide to Anti-Money Laundering (AML) and Combating the Financing of Terrorism (CFT) for banks in Singapore. It outlines the requirements and guidelines for banks to prevent and detect money laundering and terrorist financing activities. The document covers various topics such as customer due diligence, risk-based approach, suspicious transaction reporting, and cross-border wire transfers. It also provides examples of suspicious transactions and guidelines for reporting them. The document is issued by the Monetary Authority of Singapore (MAS) and is intended to help banks comply with the AML/CFT laws and regulations in Singapore.\n\n        ',NULL,'Anti Money Laundering');
+INSERT INTO `documents` VALUES ('05fec96f-5a42-4d48-b188-278836398476','pillar3-disclosures-1q-2018.pdf','2025-03-11 18:07:44','completed',NULL,NULL,'Risk Management',0.9071312436354916,NULL,NULL),('568360f5-13d0-4655-9429-ba9029138c74','626 Banks_GCO vetted.pdf','2025-03-11 18:07:43','completed','The document provided appears to be a set of guidelines issued by the Monetary Authority of Singapore (MAS) regarding the prevention of money laundering and countering the financing of terrorism. The guidelines outline the requirements and obligations for banks in Singapore to take measures to help mitigate the risk of money laundering and terrorist financing in the banking system.\n\nThe document covers various topics, including customer due diligence (CDD) measures, risk-based customization of CDD measures, simplified CDD, and enhanced CDD for high-risk customers. It also discusses the requirements for establishing correspondent banking relations, record-keeping, and reporting suspicious transactions.\n\nThe language and terminology used in the document are technical and specific to the financial industry, indicating that it is a regulatory document intended for banks and financial institutions.\n\nBased on the content and language of the document, I would classify it under the topic of \"Anti Money Laundering\".\n\n\nFinal Topic: Anti Money Laundering',NULL,'Anti Money Laundering',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `documents` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,4 +91,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-01 22:19:25
+-- Dump completed on 2025-03-11 18:33:38
