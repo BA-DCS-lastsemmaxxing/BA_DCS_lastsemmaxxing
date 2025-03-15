8ee4f3f54a87ce3e3442368eff3ffe2b0acc7f35
nicamanas <xyukimiku@gmail.com>
2025-03-16 01:54:08 +0800

    fix rds_init_lambda ref

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -120,7 +120,7 @@ resource "aws_s3_object" "rds_init_script" {
 
 resource "aws_s3_object" "rds_init_lambda_zip" {
   bucket = aws_s3_bucket.serverless_bucket_ap.bucket
-  key = "rds_init_lambda.zip"
+  key = "rds_init.zip"
 }
 
 // Lambda Layer
