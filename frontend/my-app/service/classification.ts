5fa68d8ec7d880563fa346a41593e4d2460401a8
nicamanas <xyukimiku@gmail.com>
2025-03-16 16:07:00 +0800

    fix url call

--- frontend/my-app/service/classification.ts
+++ frontend/my-app/service/classification.ts
@@ -104,7 +104,12 @@ export async function searchDocuments(query: string) {
 }
 
 export async function getDownloadLink(documentId: string) {
-    const response = await fetch(`${api.backendUrl}/download/url/${documentId}`);
+    const response = await fetch(`${api.backendUrl}/download/url?file_id=${documentId}`, {
+        method : "GET",
+        headers: {
+            "Authorization": `${getCookie("CognitoToken")}`
+        }
+});
     
     if (!response.ok) {
       throw new Error(`Failed to get download link. Status: ${response.status}`);
