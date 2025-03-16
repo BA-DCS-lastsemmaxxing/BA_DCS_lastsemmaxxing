2d36e19559452574f41275c5d25b19dae567b9ee
Khidir123 <khidirla.2022@scis.smu.edu.sg>
2025-03-16 13:35:30 +0800

    Update DocumentModal.tsx

--- frontend/my-app/components/dashboard/DocumentModal.tsx
+++ frontend/my-app/components/dashboard/DocumentModal.tsx
@@ -63,7 +63,7 @@ export function DocumentModal({ isOpen, onOpenChange, document }: DocumentModalP
     // https://kay8ehgv4g.execute-api.ap-southeast-1.amazonaws.com/Test/
     // http://127.0.0.1:5001/
     try {
-      const response = await fetch(`https://kay8ehgv4g.execute-api.ap-southeast-1.amazonaws.com/Test/send_feedback/send_feedback/?document_id=${encodeURIComponent(String(document.id))}`, {
+      const response = await fetch(`https://kay8ehgv4g.execute-api.ap-southeast-1.amazonaws.com/Test/send_feedback/?document_id=${encodeURIComponent(String(document.id))}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
