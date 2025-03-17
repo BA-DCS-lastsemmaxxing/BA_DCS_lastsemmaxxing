e6e826a16e498efb058cb4e04dc93ec09b88bbb7
nicamanas <xyukimiku@gmail.com>
2025-03-17 15:59:53 +0800

    add missing dependency

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -30,6 +30,7 @@ resource "aws_api_gateway_deployment" "prod_deployment" {
 
   depends_on = [
     aws_api_gateway_method.documents_method_get,
+    aws_api_gateway_method.documents_method_delete,
     aws_api_gateway_method.documents_method_options,
     aws_api_gateway_method.upload_url_method_get,
     aws_api_gateway_method.upload_url_method_options,
