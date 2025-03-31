77544d1e782cae49f315ff4c6bc5549ec1cb8c6c
nicamanas <xyukimiku@gmail.com>
2025-04-01 00:58:40 +0800

    bedrock vpc endpoint

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -138,3 +138,42 @@ resource "aws_security_group" "step_function_sg" {
     }
 }
 
+# VPC endpoint for bedrock
+resource "aws_vpc_endpoint" "bedrock_endpoint" {
+  vpc_id            = aws_vpc.private_vpc.id
+  service_name      = "com.amazonaws.${var.region}.bedrock"
+  vpc_endpoint_type = "Interface"
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+  security_group_ids = [aws_security_group.bedrock_sg.id]
+  private_dns_enabled = true
+
+  tags = {
+    Name = "Bedrock VPC Endpoint"
+  }
+}
+
+resource "aws_security_group" "bedrock_sg" {
+    name   = "${var.project_name}-bedrock-sg"
+  vpc_id = aws_vpc.private_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lambda_sg.id]  # Allow Lambda to reach Bedrock
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Bedrock Security Group"
+  }
+}
