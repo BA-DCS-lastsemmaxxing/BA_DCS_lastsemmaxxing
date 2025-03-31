59d1a3a1dfac97847de67b363d80eb7acfc254b7
nicamanas <xyukimiku@gmail.com>
2025-03-31 08:53:00 +0800

    change s3 bucket naming for testing purposes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,5 +7,5 @@ variable "region" {
 variable "project_name" {
   description = "The name of the project to use for naming resources"
   type        = string
-  default     = "lsm-fyp"
+  default     = "test-lsm-bucket"
 }
