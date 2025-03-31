0c54a4302efea2a5251c83275b8dc9303b910c6c
nicamanas <xyukimiku@gmail.com>
2025-03-31 09:35:04 +0800

    fix for circular dependency

--- terraform/database.tf
+++ terraform/database.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "rds_sg" {
         from_port = 3306
         to_port = 3306
         protocol = "tcp"
-        cidr_blocks = [aws_security_group.lambda_sg.id] 
+        cidr_blocks = ["172.28.0.0/16"] 
     }
 
     egress {
@@ -72,4 +72,13 @@ resource "aws_security_group" "rds_sg" {
     tags = {
         Name = "RDS Security Group"
     }
+}
+
+resource "aws_security_group_rule" "rds_ingress_from_lambda" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.lambda_sg.id
 }
