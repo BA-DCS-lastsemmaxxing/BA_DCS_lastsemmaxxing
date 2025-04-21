60dda98fc5f5ba2827aacd6bfac3d7f8e9b55c12
nicamanas <xyukimiku@gmail.com>
2025-04-21 23:36:18 +0800

    fix for missing method

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -35,7 +35,7 @@ resource "aws_api_gateway_deployment" "prod_deployment" {
     aws_api_gateway_method.upload_url_method_get,
     aws_api_gateway_method.download_url_method_get,
     aws_api_gateway_method.documents_corrected_method_get,
-    aws_api_gateway_method.feedback_method_get
+    aws_api_gateway_method.feedback_method_post
   ]
 }
 
